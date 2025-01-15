b5e11db6b79fca1999a89be65608e4cfa1986072
Tristan <123.tristan.shah@gmail.com>
2025-01-14 22:49:27 -0500

    worked if you uncomment. doing more major changes

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from copy import deepcopy
 
 import gymnasium as gym
+from gymnasium.spaces import Box
+
 from torchrl.data import ReplayBuffer
-from torchrl.data import LazyTensorStorage, ListStorage
+from torchrl.data import LazyTensorStorage
 import torch
 from torch import nn, Tensor
 
-# We define the maximum size of the buffer
-size = 1000
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
-def mlp(in_size: int, h_size: int, h_layers: int, out_size: int):
+def build_mlp(in_size: int, h_size: int, h_layers: int, out_size: int):
+    '''
+    Simple function which builds a multi-layered-perceptron neural network with an arbitrary number of hidden layers with a fixed size.
+    '''
     layers = [
         nn.Linear(in_size, h_size),
         nn.ReLU()
@@ -25,91 +31,241 @@ def mlp(in_size: int, h_size: int, h_layers: int, out_size: int):
 
     return nn.Sequential(*layers)
 
-class SAC:
+from torch.distributions import Normal, MultivariateNormal
+
+class SAC(nn.Module):
     def __init__(
             self, 
             observation_size: int,
-            action_size: int,
+            action_space: Box,
             h_size: int,
-            buffer_size: int):
-        
-        self.policy = mlp(observation_size, h_size, 2, 2 * action_size)
-        self.Q1 = mlp(observation_size + action_size, h_size, 3, 1)
-        self.Q2 = mlp(observation_size + action_size, h_size, 3, 1)
+            buffer_size: int,
+            gamma: float = 0.98,
+            alpha: float = 0.5,
+            tau: float = 0.005,
+        ):
+
+        super().__init__()
+
+        ## extracting information from action_space
+        action_size = action_space.shape[0]
+        self.register_buffer('low', torch.tensor(action_space.low))
+        self.register_buffer('high', torch.tensor(action_space.high))
+
+        ## sac hyperparams
+        self.gamma = gamma
+        self.alpha = alpha
+        self.tau = tau
+
+        ## build networks
+        self.policy = build_mlp(observation_size, h_size, 2, 2 * action_size)
+        self.Q1 = build_mlp(observation_size + action_size, h_size, 3, 1)
+        self.Q2 = build_mlp(observation_size + action_size, h_size, 3, 1)
         self.Q1_target = deepcopy(self.Q1)
         self.Q2_target = deepcopy(self.Q2)
 
+        ## set up optimizers
+        self.policy_opt = torch.optim.Adam(self.policy.parameters())
+        self.Q1_opt = torch.optim.Adam(self.Q1.parameters())
+        self.Q2_opt = torch.optim.Adam(self.Q2.parameters())
+
+        ## initialize buffer with capacity
         self.buffer = ReplayBuffer(storage = LazyTensorStorage(buffer_size))
 
-    def select_action(self, observation: Tensor):
-        mu, logvar = self.policy(observation).chunk(2, dim = 0)
+    @property
+    def device(self):
+        return self.low.device
+
+    def select_action(self, s: Tensor, prob: bool = False):
+        '''
+        Takes in a single observation or a batch and selects action(s)
+        '''
+        mu, logvar = self.policy(s).chunk(2, dim = -1)
         std = torch.exp(0.5 * logvar)
         eps = torch.randn_like(std)
+        u = mu + std * eps
 
-        a = mu + std * eps
-        print(a)
-        return
-    
-    def entropy(self, observation: Tensor, action: Tensor):
-        mu, logvar = self.policy(observation).chunk(2, dim = 0)
-        std = torch.exp(0.5 * logvar)
-        entropy = 0.5 * (1 + torch.log(2 * torch.pi)) + torch.log(std)
-        print(entropy)
+        delta = (self.high - self.low)
+        a = (torch.tanh(u) + 1.0) / 2.0 * delta + self.low
 
+        dist = Normal(mu, std)
+        
+        ## probability under a normal distribution with diagonal covariance of the unbounded u
+        log_prob_u = dist.log_prob(u).sum(dim = 1, keepdims = True)
+
+        '''
+        Need to correct the tanh squishing operation with the change of variables trick:
+
+        pi is our policy distribution and N is our unbounded normal distribution.
+
+        pi(a|s) = N(u|s) * |det(da/du)|^-1
+
+        Apply log to both sides:
+        ln(pi(a|s)) = ln(N(u|s)) - ln(|det(da/du)|)
+        '''
+
+        log_det_jac = torch.sum( torch.log( torch.abs( (1.0 - a.pow(2))/2.0 * delta ) ), dim = 1, keepdim = True)
+
+        log_prob = log_prob_u - log_det_jac
+
+        if prob:
+            return a, log_prob
+        else:
+            return a
+
+    def update(self):
+
+        batch = self.buffer.sample(64)
+
+        s   = batch['s'].to(self.device)
+        a   = batch['a'].to(self.device)
+        r   = batch['r'].to(self.device)
+        t   = batch['t'].to(self.device)
+        s_  = batch['s_'].to(self.device)
+
+        with torch.no_grad():
+            ## select an action in the next state
+            # mu, logvar = self.policy(s_).chunk(2, dim = 1)
+            # std = torch.exp(0.5 * logvar)
+            # eps = torch.randn_like(std)
+            # u_ = mu + std * eps ## unbounded u_ (-inf, inf)
+            # a_ = torch.tanh(u_)
+            a_, log_prob = self.select_action(s_, prob = True)
+
+        #     ## compute the probability of the action under a standard normal distribution
+        #     next_a_unbounded_log_prob = -0.5 * (logvar + torch.log(torch.tensor(2 * torch.pi))).sum(dim = 1, keepdim = True)
+        #     next_a_unbounded_log_prob = next_a_unbounded_log_prob.unsqueeze(-1)
+
+        #     next_a_bounded_log_prob = next_a_unbounded_log_prob - torch.log(1 - a_bounded_.pow(2) + 1e-6).sum(dim=-1, keepdim = True)
+
+        #     ## compute the value of this action using the least optimistic critic
+        #     s_a_ = torch.cat([s_, a_bounded_], dim = 1)
+        #     next_q = torch.minimum(self.Q1_target(s_a_), self.Q2_target(s_a_))
+
+        #     ## target should be the current reward plus the discounted soft q estimate (future value and policy entropy)
+        #     y = r + (~t) * self.gamma * (next_q - self.alpha * next_a_bounded_log_prob)
+
+        # ## state action pair from data
+        # sa = torch.cat([s, a], dim = 1)
+
+        # ## update state-action value functions
+        # q1_loss = torch.mean(torch.square(self.Q1(sa) - y))
+        # self.Q1_opt.zero_grad()
+        # q1_loss.backward()
+        # self.Q1_opt.step()
+
+        # q2_loss = torch.mean(torch.square(self.Q2(sa) - y))
+        # self.Q2_opt.zero_grad()
+        # q2_loss.backward()
+        # self.Q2_opt.step()
+
+        # mu, logvar = self.policy(s).chunk(2, dim = 1)
+        # std = torch.exp(0.5 * logvar)
+        # eps = torch.randn_like(std)
+        # a_unbounded = mu + std * eps
+        # a_bounded = torch.tanh(a_unbounded)
+
+        # a_unbounded_logprob = -0.5 * (logvar + torch.log(torch.tensor(2 * torch.pi))).sum(dim=1, keepdim=True)
+        # a_unbounded_logprob = a_unbounded_logprob.unsqueeze(-1)
+        # a_bounded_logprob = a_unbounded_logprob - torch.log(1 - a_bounded.pow(2) + 1e-6).sum(dim = -1, keepdim = True)
+
+        # ## state action pair with fresh action sampled in the update loop
+        # sa = torch.cat([s, a_bounded], dim = 1)
+        # q_value = torch.minimum(self.Q1(sa), self.Q2(sa))
+        # policy_loss = (self.alpha * a_bounded_logprob - q_value).mean()
+        # self.policy_opt.zero_grad()
+        # policy_loss.backward()
+        # self.policy_opt.step()
+
+        # # Soft update target networks
+        # for target_param, param in zip(self.Q1_target.parameters(), self.Q1.parameters()):
+        #     target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
+        # for target_param, param in zip(self.Q2_target.parameters(), self.Q2.parameters()):
+        #     target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
+
+        # return {'q1_loss': q1_loss.item(), 'q2_loss': q2_loss.item(), 'a_min': a.min().item(), 'a_max': a.max().item(), 'a_mean': a.mean().item()}
 
 if __name__ == '__main__':
 
-    render_mode = None #'human'
-    env = gym.make("LunarLander-v3", continuous = True, render_mode = render_mode)
+    device = torch.device(0)
 
+    # render_mode = 'human'
+    render_mode = None
+    env = gym.make('LunarLander-v3', continuous = True, render_mode = render_mode)
+    # env = gym.make('Pendulum-v1', render_mode = render_mode)
 
     state_size = env.observation_space.shape[0]
-    action_size = env.action_space.shape[0]
     h_size = 128
 
     sac = SAC(
         observation_size = env.observation_space.shape[0],
-        action_size = env.action_space.shape[0],
+        action_space = env.action_space,
         h_size = 128,
-        buffer_size = 1000
-    )
+        buffer_size = 10000
+    ).to(device)
 
     observation, info = env.reset()
 
-    observation = torch.tensor(observation)
-    a = sac.select_action(observation)
+    observation = torch.tensor(observation, device=device)
 
-    print(a)
+    observation = torch.randn(32, observation.shape[0], device = device)
+    sac.select_action(observation, prob = True)
 
 
-    # for episode in range(10):
+
+    # hist = []
+    # reward_hist = []
+
+    # for episode in range(2):
 
     #     observation, info = env.reset()
     #     episode_over = False
+    #     episode_reward = 0.0
 
     #     while not episode_over:
 
-    #         action = env.action_space.sample()  # agent policy that uses the observation and info
-    #         next_observation, reward, terminated, truncated, info = env.step(action)
-    #         episode_over = terminated or truncated
+    #         observation = torch.tensor(observation)
 
-    #         buffer.add({
-    #             's': torch.tensor(observation), 
+    #         with torch.no_grad():
+    #             action = sac.select_action(
+    #                 observation.to(device)
+    #             ).cpu().numpy()
+
+    #         next_observation, reward, terminated, truncated, _ = env.step(action)
+    #         episode_over = terminated or truncated
+    #         episode_reward += reward
+
+    #         sac.buffer.add({
+    #             's': observation, 
     #             'a': torch.tensor(action),
-    #             'r': torch.tensor(reward),
-    #             't': torch.tensor(episode_over),
+    #             'r': torch.tensor([reward]),
+    #             't': torch.tensor([episode_over]),
     #             's_': torch.tensor(next_observation)})
 
     #         observation = next_observation
 
+    #         if len(sac.buffer) > 128:
+    #             info = sac.update()
+    #             hist.append(info)
+            
+    #     print(f'Episode: {episode}, Total Reward: {episode_reward}')
+    #     reward_hist.append(episode_reward)
+
     # env.close()
 
-    # batch = buffer.sample(64)
 
-    # s = batch['s']
-    # a = batch['a']
-    # r = batch['r']
-    # t = batch['t']
-    # s_ = batch['s_']
+    # # hist = pd.DataFrame(hist)
 
-    # print(len(buffer))
+    # # fig, ax = plt.subplots(1, 3, figsize = (5, 15))
+
+    # # ax[0].set_title('Log Critic Loss')
+    # # ax[0].plot(np.log(hist['q1_loss']))
+    # # ax[0].plot(np.log(hist['q2_loss']))
+
+    # # ax[1].set_title('Reward Per Episode')
+    # # ax[1].plot(reward_hist)
+
+    # # ax[2].plot(hist['a_min'])
+    # # ax[2].plot(hist['a_mean'])
+    # # ax[2].plot(hist['a_max'])
+    # # plt.show()
